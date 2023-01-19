6b78c205a1cac94c5dc8ea75cdb139cf15ca0dca
Can Eldem <eldemcan@users.noreply.github.com>
2023-01-19 00:59:23 +0000

    use REACT_APP_ prefix for api

--- api/_utils/supabaseClient.ts
+++ api/_utils/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import * as dotenv from 'dotenv';
 
 dotenv.config()
-const supabaseUrl = process.env.REACT_SUPABASE_URL || "";
-const supabaseKey = process.env.REACT_SUPABASE_KEY || "";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "";
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
